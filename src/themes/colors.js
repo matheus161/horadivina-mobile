9f8dc6c5e21d1bf75bb746692dafb780c563031f
Matheus Lima <matheeus161@gmail.com>
2023-06-27 17:36:43 -0300

    added login and welcome pages, projetct colors and insert logo

---
+++ src/themes/colors.js
@@ -0,0 +1,18 @@
+const colors = {
+  fontPrimary: "#fff",
+  fontSecondary: "#000",
+  appPrimary: "#3d423c",
+  statusBar: "#191719",
+  white: "#fff",
+  black: "#000",
+  error: "#d73a49",
+  success: "#53a653",
+  disabled: "#6c6c6c",
+  tabBarBackground: "#19171a",
+  tagBackground: "#0D0016",
+  highlight: "#9563AB",
+  primaryLight: "#571773",
+  listsSearchBackground: "rgba(255, 255, 255, 0.6)",
+};
+
+export default colors;
